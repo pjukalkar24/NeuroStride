ee008fa163cc2caaac717ab140cacb50d83237dd
Preetam Jukalkar <preetamjukalkar@gmail.com>
2023-12-22 11:41:18 -0500

    Update alignment file and create dummy data

--- sensor_alignment.py
+++ sensor_alignment.py
@@ -123,14 +123,12 @@ class ICM20948(object):
   def __init__(self,address=I2C_ADD_ICM20948):
     self._address = address
     self._bus = smbus.SMBus(1)
-    bRet=self.icm20948Check()             #Initialization of the device 
-multiple times after power on will result in a return error
+    bRet=self.icm20948Check()             #Initialization of the device multiple times after power on will result in a return error
     # while true != bRet:
     #   print("ICM-20948 Error\n" )
     #   time.sleep(0.5)
     # print("ICM-20948 OK\n" )
-    time.sleep(0.5)                       #We can skip this detection by 
-delaying it by 500 milliseconds
+    time.sleep(0.5)                       #We can skip this detection by delaying it by 500 milliseconds
     # user bank 0 register 
     self._write_byte( REG_ADD_REG_BANK_SEL , REG_VAL_REG_BANK_0)
     self._write_byte( REG_ADD_PWR_MIGMT_1 , REG_VAL_ALL_RGE_RESET)
@@ -139,19 +137,15 @@ delaying it by 500 milliseconds
     #user bank 2 register
     self._write_byte( REG_ADD_REG_BANK_SEL , REG_VAL_REG_BANK_2)
     self._write_byte( REG_ADD_GYRO_SMPLRT_DIV , 0x07)
-    self._write_byte( REG_ADD_GYRO_CONFIG_1 , REG_VAL_BIT_GYRO_DLPCFG_6 | 
-REG_VAL_BIT_GYRO_FS_1000DPS | REG_VAL_BIT_GYRO_DLPF)
+    self._write_byte( REG_ADD_GYRO_CONFIG_1 , REG_VAL_BIT_GYRO_DLPCFG_6 | REG_VAL_BIT_GYRO_FS_1000DPS | REG_VAL_BIT_GYRO_DLPF)
     self._write_byte( REG_ADD_ACCEL_SMPLRT_DIV_2 ,  0x07)
-    self._write_byte( REG_ADD_ACCEL_CONFIG , REG_VAL_BIT_ACCEL_DLPCFG_6 | 
-REG_VAL_BIT_ACCEL_FS_2g | REG_VAL_BIT_ACCEL_DLPF)
+    self._write_byte( REG_ADD_ACCEL_CONFIG , REG_VAL_BIT_ACCEL_DLPCFG_6 | REG_VAL_BIT_ACCEL_FS_2g | REG_VAL_BIT_ACCEL_DLPF)
     #user bank 0 register
     self._write_byte( REG_ADD_REG_BANK_SEL , REG_VAL_REG_BANK_0) 
     time.sleep(0.1)
     self.icm20948GyroOffset()
     self.icm20948MagCheck()
-    self.icm20948WriteSecondary( 
-I2C_ADD_ICM20948_AK09916|I2C_ADD_ICM20948_AK09916_WRITE,REG_ADD_MAG_CNTL2, 
-REG_VAL_MAG_MODE_20HZ)
+    self.icm20948WriteSecondary( I2C_ADD_ICM20948_AK09916|I2C_ADD_ICM20948_AK09916_WRITE,REG_ADD_MAG_CNTL2, REG_VAL_MAG_MODE_20HZ)
   def icm20948_Gyro_Accel_Read(self):
     self._write_byte( REG_ADD_REG_BANK_SEL , REG_VAL_REG_BANK_0)
     data =self._read_block(REG_ADD_ACCEL_XOUT_H, 12)
@@ -162,8 +156,9 @@ REG_VAL_MAG_MODE_20HZ)
     Gyro[0]  = ((data[6]<<8)|data[7]) - GyroOffset[0]
     Gyro[1]  = ((data[8]<<8)|data[9]) - GyroOffset[1]
     Gyro[2]  = ((data[10]<<8)|data[11]) - GyroOffset[2]
-    if Accel[0]>=32767:             #Solve the problem that Python shift 
-will not overflow
+    
+    # solve the problem that python shift will not overflow
+    if Accel[0]>=32767:
       Accel[0]=Accel[0]-65535
     elif Accel[0]<=-32767:
       Accel[0]=Accel[0]+65535
@@ -187,32 +182,30 @@ will not overflow
       Gyro[2]=Gyro[2]-65535
     elif Gyro[2]<=-32767:
       Gyro[2]=Gyro[2]+65535
+
   def icm20948MagRead(self):
     counter=20
     while(counter>0):
       time.sleep(0.01)
-      self.icm20948ReadSecondary( 
-I2C_ADD_ICM20948_AK09916|I2C_ADD_ICM20948_AK09916_READ , REG_ADD_MAG_ST2, 
-1)
+      self.icm20948ReadSecondary( I2C_ADD_ICM20948_AK09916|I2C_ADD_ICM20948_AK09916_READ , REG_ADD_MAG_ST2, 1)
       if ((pu8data[0] & 0x01)!= 0):
         break
       counter-=1
     if counter!=0:
       for i in range(0,8):
-        self.icm20948ReadSecondary( 
-I2C_ADD_ICM20948_AK09916|I2C_ADD_ICM20948_AK09916_READ , REG_ADD_MAG_DATA 
-, MAG_DATA_LEN)
+        self.icm20948ReadSecondary( I2C_ADD_ICM20948_AK09916|I2C_ADD_ICM20948_AK09916_READ , REG_ADD_MAG_DATA , MAG_DATA_LEN)
         U8tempX[i] = (pu8data[1]<<8)|pu8data[0]
         U8tempY[i] = (pu8data[3]<<8)|pu8data[2]
         U8tempZ[i] = (pu8data[5]<<8)|pu8data[4]
       
-Mag[0]=(U8tempX[0]+U8tempX[1]+U8tempX[2]+U8tempX[3]+U8tempX[4]+U8tempX[5]+U8tempX[6]+U8tempX[7])/8
-      
-Mag[1]=-(U8tempY[0]+U8tempY[1]+U8tempY[2]+U8tempY[3]+U8tempY[4]+U8tempY[5]+U8tempY[6]+U8tempY[7])/8
-      
-Mag[2]=-(U8tempZ[0]+U8tempZ[1]+U8tempZ[2]+U8tempZ[3]+U8tempZ[4]+U8tempZ[5]+U8tempZ[6]+U8tempZ[7])/8
-    if Mag[0]>=32767:            #Solve the problem that Python shift will 
-not overflow
+    Mag[0]=(U8tempX[0]+U8tempX[1]+U8tempX[2]+U8tempX[3]+U8tempX[4]+U8tempX[5]+U8tempX[6]+U8tempX[7])/8
+
+    Mag[1]=-(U8tempY[0]+U8tempY[1]+U8tempY[2]+U8tempY[3]+U8tempY[4]+U8tempY[5]+U8tempY[6]+U8tempY[7])/8
+
+    Mag[2]=-(U8tempZ[0]+U8tempZ[1]+U8tempZ[2]+U8tempZ[3]+U8tempZ[4]+U8tempZ[5]+U8tempZ[6]+U8tempZ[7])/8
+    
+    if Mag[0]>=32767:
+      # solve overflow
       Mag[0]=Mag[0]-65535
     elif Mag[0]<=-32767:
       Mag[0]=Mag[0]+65535
@@ -224,16 +217,15 @@ not overflow
       Mag[2]=Mag[2]-65535
     elif Mag[2]<=-32767:
       Mag[2]=Mag[2]+65535
+  
   def icm20948ReadSecondary(self,u8I2CAddr,u8RegAddr,u8Len):
     u8Temp=0
-    self._write_byte( REG_ADD_REG_BANK_SEL,  REG_VAL_REG_BANK_3) #swtich 
-bank3
+    self._write_byte( REG_ADD_REG_BANK_SEL,  REG_VAL_REG_BANK_3) #switch bank3
     self._write_byte( REG_ADD_I2C_SLV0_ADDR, u8I2CAddr)
     self._write_byte( REG_ADD_I2C_SLV0_REG,  u8RegAddr)
     self._write_byte( REG_ADD_I2C_SLV0_CTRL, REG_VAL_BIT_SLV0_EN|u8Len)
 
-    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_0) #swtich 
-bank0
+    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_0) #switch bank0
     
     u8Temp = self._read_byte(REG_ADD_USER_CTRL)
     u8Temp |= REG_VAL_BIT_I2C_MST_EN
@@ -245,26 +237,23 @@ bank0
     for i in range(0,u8Len):
       pu8data[i]= self._read_byte( REG_ADD_EXT_SENS_DATA_00+i)
 
-    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_3) #swtich 
-bank3
+    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_3) #switch bank3
     
     u8Temp = self._read_byte(REG_ADD_I2C_SLV0_CTRL)
     u8Temp &= ~((REG_VAL_BIT_I2C_MST_EN)&(REG_VAL_BIT_MASK_LEN))
     self._write_byte( REG_ADD_I2C_SLV0_CTRL,  u8Temp)
     
-    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_0) #swtich 
-bank0
+    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_0) #switch bank0
+
   def icm20948WriteSecondary(self,u8I2CAddr,u8RegAddr,u8data):
     u8Temp=0
-    self._write_byte( REG_ADD_REG_BANK_SEL,  REG_VAL_REG_BANK_3) #swtich 
-bank3
+    self._write_byte( REG_ADD_REG_BANK_SEL,  REG_VAL_REG_BANK_3) #switch bank3
     self._write_byte( REG_ADD_I2C_SLV1_ADDR, u8I2CAddr)
     self._write_byte( REG_ADD_I2C_SLV1_REG,  u8RegAddr)
     self._write_byte( REG_ADD_I2C_SLV1_DO,   u8data)
     self._write_byte( REG_ADD_I2C_SLV1_CTRL, REG_VAL_BIT_SLV0_EN|1)
 
-    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_0) #swtich 
-bank0
+    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_0) #switch bank0
 
     u8Temp = self._read_byte(REG_ADD_USER_CTRL)
     u8Temp |= REG_VAL_BIT_I2C_MST_EN
@@ -273,15 +262,14 @@ bank0
     u8Temp &= ~REG_VAL_BIT_I2C_MST_EN
     self._write_byte( REG_ADD_USER_CTRL, u8Temp)
 
-    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_3) #swtich 
-bank3
+    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_3) #switch bank3
 
     u8Temp = self._read_byte(REG_ADD_I2C_SLV0_CTRL)
     u8Temp &= ~((REG_VAL_BIT_I2C_MST_EN)&(REG_VAL_BIT_MASK_LEN))
     self._write_byte( REG_ADD_I2C_SLV0_CTRL,  u8Temp)
 
-    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_0) #swtich 
-bank0
+    self._write_byte( REG_ADD_REG_BANK_SEL, REG_VAL_REG_BANK_0) #switch bank0
+
   def icm20948GyroOffset(self):
     s32TempGx = 0
     s32TempGy = 0
@@ -295,17 +283,22 @@ bank0
     GyroOffset[0] = s32TempGx >> 5
     GyroOffset[1] = s32TempGy >> 5
     GyroOffset[2] = s32TempGz >> 5
+
   def _read_byte(self,cmd):
     return self._bus.read_byte_data(self._address,cmd)
+
   def _read_block(self, reg, length=1):
     return self._bus.read_i2c_block_data(self._address, reg, length)
+
   def _read_u16(self,cmd):
     LSB = self._bus.read_byte_data(self._address,cmd)
     MSB = self._bus.read_byte_data(self._address,cmd+1)
     return (MSB	<< 8) + LSB
+
   def _write_byte(self,cmd,val):
     self._bus.write_byte_data(self._address,cmd,val)
     time.sleep(0.0001)
+
   def imuAHRSupdate(self,gx, gy,gz,ax,ay,az,mx,my,mz):    
     norm=0.0
     hx = hy = hz = bx = bz = 0.0
@@ -339,12 +332,9 @@ bank0
     mz = mz * norm
 
     # compute reference direction of flux
-    hx = 2 * mx * (0.5 - q2q2 - q3q3) + 2 * my * (q1q2 - q0q3) + 2 * mz * 
-(q1q3 + q0q2)
-    hy = 2 * mx * (q1q2 + q0q3) + 2 * my * (0.5 - q1q1 - q3q3) + 2 * mz * 
-(q2q3 - q0q1)
-    hz = 2 * mx * (q1q3 - q0q2) + 2 * my * (q2q3 + q0q1) + 2 * mz * (0.5 - 
-q1q1 - q2q2)         
+    hx = 2 * mx * (0.5 - q2q2 - q3q3) + 2 * my * (q1q2 - q0q3) + 2 * mz * (q1q3 + q0q2)
+    hy = 2 * mx * (q1q2 + q0q3) + 2 * my * (0.5 - q1q1 - q3q3) + 2 * mz * (q2q3 - q0q1)
+    hz = 2 * mx * (q1q3 - q0q2) + 2 * my * (q2q3 + q0q1) + 2 * mz * (0.5 - q1q1 - q2q2)         
     bx = math.sqrt((hx * hx) + (hy * hy))
     bz = hz     
 
@@ -356,8 +346,7 @@ q1q1 - q2q2)
     wy = 2 * bx * (q1q2 - q0q3) + 2 * bz * (q0q1 + q2q3)
     wz = 2 * bx * (q0q2 + q1q3) + 2 * bz * (0.5 - q1q1 - q2q2)  
 
-    # error is sum of cross product between reference direction of fields 
-and direction measured by sensors
+    # error is sum of cross product between reference direction of fields and direction measured by sensors
     ex = (ay * vz - az * vy) + (my * wz - mz * wy)
     ey = (az * vx - ax * vz) + (mz * wx - mx * wz)
     ez = (ax * vy - ay * vx) + (mx * wy - my * wx)
@@ -381,19 +370,19 @@ and direction measured by sensors
     q1 = q1 * norm
     q2 = q2 * norm
     q3 = q3 * norm
+
   def icm20948Check(self):
     bRet=false
     if REG_VAL_WIA == self._read_byte(REG_ADD_WIA):
       bRet = true
     return bRet
+
   def icm20948MagCheck(self):
-    self.icm20948ReadSecondary( 
-I2C_ADD_ICM20948_AK09916|I2C_ADD_ICM20948_AK09916_READ,REG_ADD_MAG_WIA1, 
-2)
-    if (pu8data[0] == REG_VAL_MAG_WIA1) and ( pu8data[1] == 
-REG_VAL_MAG_WIA2) :
+    self.icm20948ReadSecondary( I2C_ADD_ICM20948_AK09916|I2C_ADD_ICM20948_AK09916_READ,REG_ADD_MAG_WIA1, 2)
+    if (pu8data[0] == REG_VAL_MAG_WIA1) and ( pu8data[1] == REG_VAL_MAG_WIA2) :
         bRet = true
         return bRet
+
   def icm20948CalAvgValue(self):
     MotionVal[0]=Gyro[0]/32.8
     MotionVal[1]=Gyro[1]/32.8
@@ -419,14 +408,13 @@ if __name__ == '__main__':
             icm20948.icm20948MagRead()
             icm20948.icm20948CalAvgValue()
             time.sleep(0.1)
-            icm20948.imuAHRSupdate(MotionVal[0] * 0.0175, MotionVal[1] * 0.0175, 
-MotionVal[2] * 0.0175, MotionVal[3], MotionVal[4], MotionVal[5], MotionVal[6], MotionVal[7], 
-MotionVal[8])
+            icm20948.imuAHRSupdate(MotionVal[0] * 0.0175, MotionVal[1] * 0.0175, MotionVal[2] * 0.0175, MotionVal[3], MotionVal[4], MotionVal[5], MotionVal[6], MotionVal[7], MotionVal[8])
             pitch = math.asin(-2 * q1 * q3 + 2 * q0* q2)* 57.3
             roll  = math.atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2* q2 + 1)* 57.3
             yaw   = math.atan2(-2 * q1 * q2 - 2 * q0 * q3, 2 * q2 * q2 + 2 * q3 * q3 - 1) * 57.3
             
             log.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}\n".format(roll, pitch, yaw, Accel[0], Accel[1], Accel[2], Gyro[0], Gyro[1], Gyro[2], Mag[0], Mag[1], Mag[2]))
+            print(x)
             x += 1
         except(KeyboardInterrupt):
             print("\n")
